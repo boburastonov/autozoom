11c5972e7a915ca95ab8b357912c4e6a344e4abe
boburastonov <boburastonov33@gmail.com>
2024-09-10 17:58:25 +0500

    add about page with media styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about md:py-[50px] py-[40px] text-white">
+      <div className="container sm:px-5 px-2">
+        <div>
+          <a className="text-[#fff] text-[14px]" href="/">
+            Luxury Cars for Rent in Dubai / aboutUs2
+          </a>
+          <h1 className="md:text-[32px] md:my-[70px] sm:my-[50px] sm:text-[28px] text-[25px] my-[35px] font-bold leading-[130%] uppercase">
+            ABOUT US
+          </h1>
+        </div>
+        <div>
+          <h2 className="text-[30px] py-[10px] leading-[40px]">
+            Welcome to Auto Zoom Car Rental Co LLC – Your Partner in
+            Unforgettable Journeys in Dubai!
+          </h2>
+          <p className="md:mb-[50px] sm:mb-[40px] mb-[30px]  text-[#cacaca] text-[19px] py-[10px]">
+            In a world where every trip is an adventure, Auto Zoom Car Rental Co
+            LLC offers you a unique car rental experience in the heart of Dubai.
+          </p>
+          <h3 className="text-[28px] mb-[15px] py-[10px] capitalize">
+            Why Auto Zoom?
+          </h3>
+          <div className="leading-[30px] py-[10px]">
+            <h4 className="inline-block text-[21px] font-semibold mr-[5px]">
+              Elegant Car Fleet:
+            </h4>
+            <p className="text-[#d8d8d8] inline">
+              We provide you the opportunity to choose from our luxurious car
+              fleet – from sleek sedans to powerful SUVs. We have a vehicle to
+              match your style and meet your needs.
+            </p>
+          </div>
+          <div className="leading-[30px] py-[10px]">
+            <h4 className="inline-block text-[21px] font-semibold mr-[5px]">
+              Worry-Free Rental:
+            </h4>
+            <p className="text-[#d8d8d8] inline">
+              Our goal is to make your journey as comfortable as possible.
+              Flexible rental terms, the option to hire with a driver, and a
+              wide range of additional services – all for your convenience.
+            </p>
+          </div>
+          <div className="leading-[30px] py-[10px]">
+            <h4 className="inline-block text-[21px] font-semibold mr-[5px]">
+              Transparency and Trust:
+            </h4>
+            <p className="text-[#d8d8d8] inline">
+              We value every customer, so our prices are honest and transparent.
+              No hidden fees, no unexpected surprises. Trust us, and we will
+              make your rental experience straightforward and clear.
+            </p>
+          </div>
+          <div className="leading-[30px] py-[10px]">
+            <h4 className="inline-block text-[21px] font-semibold mr-[5px]">
+              Service at Your Level:
+            </h4>
+            <p className="text-[#d8d8d8] inline">
+              Our friendly and professional team is ready to bring your ideas to
+              life. We aim not just to provide a car but to create a genuine
+              automotive experience for you.
+            </p>
+          </div>
+          <div className="leading-[30px] py-[10px]">
+            <h4 className="inline-block text-[21px] font-semibold mr-[5px]">
+              Our Mission:
+            </h4>{" "}
+            <br />
+            <p className="text-[#d8d8d8] inline mb-5">
+              Auto Zoom Car Rental Co LLC is created with one goal – to make
+              every journey unforgettable. We strive to offer you not just a car
+              but the key to new experiences and adventures.
+            </p>
+          </div>
+          <div className="leading-[30px] py-[10px]">
+            <h4 className="inline-block text-[21px] font-semibold mr-[5px]">
+              Contact Us:
+            </h4>{" "}
+            <br />
+            <p className="text-[#d8d8d8] inline mb-5">
+              Trust the professionals – trust Auto Zoom Car Rental Co LLC. Get
+              in touch with us today to start your exciting journey through
+              Dubai in our stylish and reliable cars. Allow yourself to savor
+              every moment of your trip.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
